085b63a1264f69dc607563ae0e88688eb0f16579
RogorZhang <rogor.zhang@gmail.com>
2023-01-12 19:57:46 -0500

    first commit

---
+++ src/main/java/org/example/day1/Day1Note.java
@@ -0,0 +1,27 @@
+package org.example.day1;
+
+public class Day1Note {
+    public static void main(String[] args) {
+/*
+1. maven
+    folder structure
+    types of repository:
+        local (.m2), central, remote
+    maven build lifecycle
+        clean, validation, compile, test, ....
+    Command Line
+        mvn clean, mvn test, mvn install
+
+2. git
+    distributed version control
+
+
+
+
+
+
+*/
+
+
+    }
+}
